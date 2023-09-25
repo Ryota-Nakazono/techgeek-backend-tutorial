31d64e2b491dbaa4de19529d84e1fcdd8f73a3c3
Ryota-Nakazono <ryota.shunpu.sfz2@gmail.com>
2023-09-25 12:45:59 +0900

    テキストの軽微な変更

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+  res.send("<h1>Hello World!</h1>");
 });
 
 app.post("/login", (req, res) => {
